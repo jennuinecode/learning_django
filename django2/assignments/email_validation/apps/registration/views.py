86ae44f402f68aa18a2c3acabc88712396b6f4b1
jennuinecode <jennifer.cortes@hotmail.com>
2017-02-28 14:32:36 -0600

    completed email registration

--- django2/assignments/email_validation/apps/registration/views.py
+++ django2/assignments/email_validation/apps/registration/views.py
@@ -2,26 +2,26 @@ from django.shortcuts import render, redirect
 import re
 from .models import User
 
+
+
 def index(request):
 
-    context = {
-        'users': User.objects.all(),
-    }
-
-    return render(request, 'registration/index.html', context)
+    return render(request, 'registration/index.html')
 
 def register(request):
+
+    errors = {
+        'users':User.objects.all(),
+    }
     print "made it to register method ****************"
 
     if not re.match( r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", request.POST['email']):
-            context = {
-                'messages': []
-            }
-            if messages not in context:
-                print "please enter valid email **************"
-                context['messages'].append("please enter valid email address")
-
+        print "please enter valid email **************"
+        errors['invalidEmail'] = "Please enter a valid email"
     else:
         User.objects.create(email=request.POST['email'])
         print "email successfully added **************"
-    return redirect('/')
+        errors['success'] = "Successfully registered"
+
+
+    return render(request, 'registration/index.html', errors)
