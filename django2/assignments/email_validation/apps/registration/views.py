eb9984fb97933c553985d8317cb44c40a5e1da4b
jennuinecode <jennifer.cortes@hotmail.com>
2017-02-28 14:06:12 -0600

    changes to email_val

--- django2/assignments/email_validation/apps/registration/views.py
+++ django2/assignments/email_validation/apps/registration/views.py
@@ -6,7 +6,6 @@ def index(request):
 
     context = {
         'users': User.objects.all(),
-        'messages': []
     }
 
     return render(request, 'registration/index.html', context)
@@ -15,8 +14,12 @@ def register(request):
     print "made it to register method ****************"
 
     if not re.match( r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", request.POST['email']):
-        print "please enter valid email **************"
-        context['messages'].append("please enter valid email address")
+            context = {
+                'messages': []
+            }
+            if messages not in context:
+                print "please enter valid email **************"
+                context['messages'].append("please enter valid email address")
 
     else:
         User.objects.create(email=request.POST['email'])
